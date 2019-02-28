1f13555cac2825d1377439b777aed9ddd6b6bbc9
github username <github email address>
2019-02-28 18:14:26 -0500

    Fleet Bug Fixes

--- app/validators/league_validator.rb
+++ app/validators/league_validator.rb
@@ -1,6 +1,7 @@
 class LeagueValidator < ActiveModel::Validator
   def validate(record)
-    if League.find(record.league_id).full?
+    league = League.find(record.league_id)
+    if league.full? && !league.fleets.include?(record)
       record.errors[:base] << "Unable to join. League is full."
     end
   end
